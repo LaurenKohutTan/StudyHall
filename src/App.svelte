75ee9db5250b994d5bf2055cee517f74064fd883
Connor Slade <connor@connorcode.com>
2023-12-07 17:49:34 -0500

    Remove unused files

--- src/App.svelte
+++ src/App.svelte
@@ -39,83 +39,78 @@ Yemula, Archit
 Yermak, George
 Yu, Angela
 Zimnoch, Kaylin
-`
-  import {savedArray} from './stores.js' 
-	let students = data.trim().split("\n").filter(
-    studentName => !$savedArray.includes(studentName)
-  );
-	let signedIn = $savedArray;
-	function removeName(name)
-	{
-    new Audio('sound.wav').play(); 
-    signedIn=[...signedIn, name];
-    $savedArray = signedIn;
-		students = students.filter(student=>student!=name);	
-	}
+`;
 
-  function reset()
-  {
+  import { savedArray } from "./stores.js";
+
+  let students = data
+    .trim()
+    .split("\n")
+    .filter((studentName) => !$savedArray.includes(studentName));
+  let signedIn = $savedArray;
+  function removeName(name) {
+    new Audio("sound.wav").play();
+    signedIn = [...signedIn, name];
+    $savedArray = signedIn;
+    students = students.filter((student) => student != name);
+  }
+
+  function reset() {
     students = data.trim().split("\n");
     signedIn = [];
     $savedArray = signedIn;
   }
 
-  function undo()
-  {
-    students.push(signedIn[signedIn.length - 1]);	
-    signedIn.splice(signedIn.length - 1, 1);	
+  function undo() {
+    students.push(signedIn[signedIn.length - 1]);
+    signedIn.splice(signedIn.length - 1, 1);
 
     students = students.sort();
     signedIn = signedIn.sort();
     $savedArray = signedIn;
   }
 </script>
-<h1> Vail Study Hall </h1>
-<button on:click={_=>undo()}>
-  UNDO LAST
-  </button>
 
-  <br>
+<h1>Vail Study Hall</h1>
+<button on:click={(_) => undo()}> UNDO LAST </button>
+
+<br />
 {#each students as name}
-<div class="btn-group">
-<button on:click={_=>removeName(name)}>
-	{name}
-</button>
-</div>
+  <div class="btn-group">
+    <button on:click={(_) => removeName(name)}>
+      {name}
+    </button>
+  </div>
 {/each}
 
-<p >
-	{$savedArray.join(", ")}
+<p>
+  {$savedArray.join(", ")}
 </p>
-<br><br><br>
-<button on:click={_=>reset()}>
-  Reset List
-  </button>
-<style>
+<br /><br /><br />
+<button on:click={(_) => reset()}> Reset List </button>
 
-.btn-group button {
-  display: block;
-  margin-bottom: 35px;
-  padding: 15px;
-		background-color: #f76027;
-		color: white;
-		border: none;
-		border-radius: 4px;
-		padding: 0.8rem;
+<style>
+  .btn-group button {
+    display: block;
+    margin-bottom: 35px;
+    padding: 15px;
+    background-color: #f76027;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 0.8rem;
     display: block;
     font-size: 24px;
-	}
-  button
-  {
+  }
+  button {
     display: block;
     margin-bottom: 15px;
     padding: 15px;
-		background-color: #CCCCCC;
-		color: black;
-		border: none; 
-		border-radius: 4px;
-		padding: 0.5rem;
+    background-color: #cccccc;
+    color: black;
+    border: none;
+    border-radius: 4px;
+    padding: 0.5rem;
     display: block;
   }
-
 </style>
